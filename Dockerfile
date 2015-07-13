f0081a242e10179be01e26475af27c3281990628
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-07-13 10:13:47 +0900

    rails new first_app and bundle install

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,7 @@ RUN apt-get -y update && apt-get -y upgrade
 RUN apt-get -y install build-essential ruby2.0 ruby2.0-dev libyaml-dev libxslt1-dev libsqlite3-dev
 RUN ln -sf $(which ruby2.0) /usr/bin/ruby && ln -sf $(which gem2.0) /usr/bin/gem
 RUN gem install rails --version 4.0.5 --no-document
+
+COPY first_app/Gemfile /tmp/first_app/
+WORKDIR /tmp/first_app
+RUN bundle install
---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '3200d248784c96517d6acff00be9313b8da5dabdc1eba703566b0e3533bd5588f42b2644fb93bb40d8c05bccc407ebdd39809aad99dcc30ae15e42523cc7706d'
