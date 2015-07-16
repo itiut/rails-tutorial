50e996dc6bc6e1051e5a46201a4d779f5e6aeae6
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-07-16 21:40:23 +0900

    Implement authorization

--- sample_app/spec/support/utilities.rb
+++ sample_app/spec/support/utilities.rb
@@ -6,8 +6,23 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
-RSpec::Matchers.define :have_error_message do |message|
-  match do |page|
-    expect(page).to have_selector('div.alert.alert-error', text: message)
+def sign_in(user, options={})
+  if options[:no_capybara]
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path
+    fill_in 'Email',    with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+  end
+end
+
+%w[error success].each do |kind|
+  RSpec::Matchers.define "have_#{kind}_message" do |message|
+    match do |page|
+      expect(page).to have_selector("div.alert.alert-#{kind}", text: message)
+    end
   end
 end
