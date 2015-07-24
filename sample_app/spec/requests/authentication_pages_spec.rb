fb81208b44978f9ae83399340df22d3c96e92fd3
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-07-25 04:14:53 +0900

    Create follow/unfollow web UI

--- sample_app/spec/requests/authentication_pages_spec.rb
+++ sample_app/spec/requests/authentication_pages_spec.rb
@@ -85,6 +85,16 @@ describe "Authentication" do
           it { should_not have_link('Settings') }
           it { should_not have_link('Sign out') }
         end
+
+        describe 'visiting the following page' do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe 'visiting the followers page' do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
       end
 
       describe 'when attenpting to visit a protected page' do
@@ -111,6 +121,18 @@ describe "Authentication" do
               expect(page).to have_title(user.name)
             end
           end
+        end
+      end
+
+      describe 'in the Relationships controller' do
+        describe 'submitting to the create action' do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe 'submitting to the destroy action' do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
         end
       end
 
